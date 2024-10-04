775c5dc57f07c1b1da0347e19ccc727ed23b89ae
TatsuyaEzaki <t-ezaki@kawahara.ac.jp>
2024-10-04 16:35:20 +0900

    エネミーのAIの基底クラスを作成

---
+++ haruharuEngine/GameTemplate/Game/EnemyAIBase.cpp
@@ -0,0 +1,2 @@
+#include "stdafx.h"
+#include "EnemyAIBase.h"
---
+++ haruharuEngine/GameTemplate/Game/EnemyAIBase.h
@@ -0,0 +1,15 @@
+#pragma once
+class EnemyAIBase
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	EnemyAIBase(){};
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	virtual ~EnemyAIBase(){};
+private:
+};
+
