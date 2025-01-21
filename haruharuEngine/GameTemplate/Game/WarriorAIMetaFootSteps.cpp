ff3b417f96e3696c0141cc51caee12b5508d3fe9
takeda haruse <haruse010655@gmail.com>
2025-01-21 13:47:03 +0900

    割り当てプログラムプロトタイプ完成、テストまだ

--- haruharuEngine/GameTemplate/Game/WarriorAIMetaFootSteps.cpp
+++ haruharuEngine/GameTemplate/Game/WarriorAIMetaFootSteps.cpp
@@ -5,7 +5,12 @@
 #include "EnemySM_Warrior.h"
 #include "EnemyBase.h"
 
+//定数等
 namespace {
+
+	static const float HIGH_VALUME_INDEX = 1.0f;
+	static const float MIDDLE_VALUME_INDEX = 0.4f;
+	static const float LOW_VALUME_INDEX = 0.3f;
 
 }
 
@@ -20,35 +25,89 @@ void WarriorAIMetaFootSteps::MetaAIExecution(EnemySMBase* initEnemy)
 {
 	m_MainCallWarrior = initEnemy;
 
+	//エネミーの数取得
+	m_enemyNum = m_sharedWarriorDatas->m_warriorDatas.size();
+
+	//距離データ初期化
+	for (int enemyNo = 0; enemyNo < m_enemyNum; enemyNo++)
+	{
+		m_enemyLengthDatas[enemyNo].m_enemyData 
+			= &m_sharedWarriorDatas->m_warriorDatas[enemyNo]->GetEnemyPtr();
+
+		m_enemyLengthDatas[enemyNo].m_length = 0.0f;
+
+		m_enemyLengthDatas[enemyNo].m_priority = en_priority_non;
+	}
+
 	LengthCalc();
+
+	DatasPriorityInit();
 }
 
 //距離計算
 void WarriorAIMetaFootSteps::LengthCalc()
 {
 
-	EnemyFootValumeData datas[3];
-
-	int enemyNum = 3;
-
 	//エネミー全体のプレイヤーとの距離を測る
-	for (int enemyNo = 0; enemyNo < enemyNum; enemyNo++)
+	for (int enemyNo = 0; enemyNo < m_enemyNum; enemyNo++)
 	{
 		
 		Vector3 diff = m_player->GetPosition() 
 			- m_sharedWarriorDatas->m_warriorDatas[enemyNo]->GetEnemyPtr().GetPosition();
 
-		datas[enemyNo].m_distance = diff.Length();
+		m_enemyLengthDatas[enemyNo].m_length = diff.Length();
 
-	}
+	}	
 
-	for ()
+}
+
+void WarriorAIMetaFootSteps::DatasPriorityInit()
+{
+
+	EnemyLength_Data* highData = nullptr;
+	EnemyLength_Data* lowData = nullptr;
+	std::vector<EnemyLength_Data*> middleData;
+	
+	//まず先頭要素のエネミーを一番近いエネミーと課程する
+	//そして格納したデータとほかのデータを
+	//比べて小さい方をhighに代入する
+	//lowも大体同じ処理
+	highData = &m_enemyLengthDatas[0];
+	lowData = &m_enemyLengthDatas[0];
+	for (int enemyNo = 0;
+		enemyNo < m_enemyNum;
+		enemyNo++)
 	{
+		if (highData->m_length
+			> m_enemyLengthDatas[enemyNo].m_length)
+			highData = &m_enemyLengthDatas[enemyNo];
 
-
-
+		if (lowData->m_length
+			< m_enemyLengthDatas[enemyNo].m_length)
+			lowData = &m_enemyLengthDatas[enemyNo];
 	}
 
+	//そしてそれ以外のデータをmiddleに指定
+	for (int enemyNo = 0;
+		enemyNo < m_enemyNum;
+		enemyNo++)
+	{
+		if (&m_enemyLengthDatas[enemyNo] == highData ||
+			&m_enemyLengthDatas[enemyNo] == lowData)
+			continue;
+		else
+			middleData.push_back(&m_enemyLengthDatas[enemyNo]);
+	}
+
+	//最終設定
+	highData->m_enemyData->SetFootStepValumeCalcValue(HIGH_VALUME_INDEX);
+
+	lowData->m_enemyData->SetFootStepValumeCalcValue(LOW_VALUME_INDEX);
+
+	for (auto& listData : middleData)
+	{
+		listData->m_enemyData->SetFootStepValumeCalcValue(MIDDLE_VALUME_INDEX);
+	}
 }
 
 //処理終了
