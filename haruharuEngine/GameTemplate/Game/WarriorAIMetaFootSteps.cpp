fb013923f2c0f869dc2a5a527afeb5caeec99628
takeda haruse <haruse010655@gmail.com>
2025-01-29 22:11:57 +0900

    足音を、壁あり、壁なしでエネミーの方を向いていない、壁なしでエネミーの方を向いている、の三種類で音を分断化することに成功

--- haruharuEngine/GameTemplate/Game/WarriorAIMetaFootSteps.cpp
+++ haruharuEngine/GameTemplate/Game/WarriorAIMetaFootSteps.cpp
@@ -8,16 +8,17 @@
 //定数等
 namespace {
 
-	static const float HIGH_VALUME_INDEX = 1.0f;
-	static const float MIDDLE_VALUME_INDEX = 0.0f;
-	static const float LOW_VALUME_INDEX = 0.0f;
-
+	static const float HIGH_VALUME_INDEX = 10.0f;
+	static const float MIDDLE_VALUME_INDEX = 7.0f;
+	static const float LOW_VALUME_INDEX = 5.0f;
 }
 
 //メタAIの初期化
 void WarriorAIMetaFootSteps::MetaAIInit()
 {
 	m_player = FindGO<Player>("player");
+	//コライダーを初期化
+	m_sphereCollider.Create(1.0f);
 }
 
 //メタAIの実行
@@ -52,6 +53,8 @@ void WarriorAIMetaFootSteps::MetaAIExecution(EnemySMBase* initEnemy)
 	LengthCalc();
 
 	DatasPriorityInit();
+
+	DatasEnemyWallCheck();
 }
 
 void WarriorAIMetaFootSteps::EnemyStateCheck()
@@ -69,7 +72,7 @@ void WarriorAIMetaFootSteps::EnemyStateCheck()
 			!= EnemySM_Warrior::en_warrior_trackingMetaAI)
 		{
 			m_enemyLengthDatas[enemyNo].m_enemyData
-				->SetFootStepValumeCalcValue(true);
+				->SetFootStepFlag(true);
 
 			return;
 		}
@@ -126,11 +129,128 @@ void WarriorAIMetaFootSteps::DatasPriorityInit()
 	}
 
 	//最終設定
-	highData->m_enemyData->SetFootStepValumeCalcValue(true);
+	highData->m_enemyData->SetFootStepFlag(true);
 
 	for (auto& listData : lowDatas)
 	{
-		listData->m_enemyData->SetFootStepValumeCalcValue(false);
+		listData->m_enemyData->SetFootStepFlag(false);
+	}
+}
+
+
+//壁の判定を取る構造体
+struct SweepResultWall : public btCollisionWorld::ConvexResultCallback
+{
+	/// <summary>
+	/// 対象属性にレイが当たったかどうか
+	/// </summary>
+	bool isHit = false;
+	/// <summary>
+	///	判定
+	/// </summary>
+	/// <param name="covexResult"></param>
+	/// <param name="normalInWorldSpace"></param>
+	/// <returns></returns>
+	virtual btScalar addSingleResult(btCollisionWorld::LocalConvexResult& covexResult,
+		bool normalInWorldSpace)
+	{
+		//壁とぶつかっていなかったら
+		if (covexResult.m_hitCollisionObject->getUserIndex() != enCollisionAttr_Wall)
+		{
+			//衝突したのは壁ではない
+			return 0.0f;
+		}
+
+		//壁とぶつかってたらフラグをtrueにする
+		isHit = true;
+		return 0.0f;
+	}
+};
+
+const bool& WarriorAIMetaFootSteps::RayTestWall(EnemyBase* initEnemy)
+{
+	//壁越しに見つけないようにレイを使用して壁の判定を取る
+	//コライダーの始点と終点を初期化
+	btTransform start, end;
+	start.setIdentity();
+	end.setIdentity();
+
+	//始点位置決定
+	Vector3 startPos = initEnemy->GetPosition();
+	startPos.y += 70.0f;
+	start.setOrigin(btVector3(startPos.x, startPos.y, startPos.z));
+
+	//終点位置決定
+	Vector3 endPos = m_player->GetPosition();
+	endPos.y += 75.0f;
+	end.setOrigin(btVector3(endPos.x, endPos.y, endPos.z));
+
+	SweepResultWall callback;
+
+	//衝突検出
+	PhysicsWorld::GetInstance()->ConvexSweepTest((const btConvexShape*)m_sphereCollider.GetBody(),
+		start, end, callback);
+	if (callback.isHit) {
+		//当たった
+		return true;
+	}
+
+	//当たってない
+	return false;
+}
+
+const bool& WarriorAIMetaFootSteps::IsinCamera(EnemyBase* initEnemy)
+{
+
+	Vector3 diff = initEnemy->GetPosition() - g_camera3D->GetPosition();
+
+	//エネミーからプレイヤーに向かうベクトルを正規化
+	diff.Normalize();
+	//エネミーの正面ベクトルと、敵からプレイヤーに向かうベクトルの
+	//内積(cosθ)を求める。
+	float cos = g_camera3D->GetForward().Dot(diff);
+	if (cos >= 1)
+	{
+		cos = 1.0f;
+	}
+	//内積(cosθ)から角度(θ)を求める
+	float angle = acosf(cos);
+	//角度(θ)が90°(視野角)より小さければ
+	if (angle <= (Math::PI / 180.0f) * 90.0f)
+	{
+
+		return true;
+
+	}
+
+	return false;
+
+}
+
+void WarriorAIMetaFootSteps::DatasEnemyWallCheck()
+{
+	
+	for (int enemyNo = 0;
+		enemyNo < m_enemyNum;
+		enemyNo++)
+	{
+
+		if (RayTestWall(m_enemyLengthDatas[enemyNo].m_enemyData))
+		{
+			m_enemyLengthDatas[enemyNo].m_enemyData->SetValumeCalcValue(LOW_VALUME_INDEX);
+
+			continue;
+		}
+
+		if (IsinCamera(m_enemyLengthDatas[enemyNo].m_enemyData))
+		{
+			m_enemyLengthDatas[enemyNo].m_enemyData->SetValumeCalcValue(HIGH_VALUME_INDEX);
+
+			continue;
+		}
+
+		m_enemyLengthDatas[enemyNo].m_enemyData->SetValumeCalcValue(MIDDLE_VALUME_INDEX);
+
 	}
 }
 
